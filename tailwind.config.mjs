2b67d53cde856bd2100fb74c114c0b844be94305
unknown <me.leandrop@gmail.com>
2024-09-15 14:52:32 -0300

    darkmode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Raleway', 'sans-serif'],
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
